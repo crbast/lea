e0d9d91440e71a8d6da3bd45d27b65653bce01fd
crbast <bastien.crettenand@hotmail.com>
2021-10-25 19:36:44 +0200

    Test TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+
+
+use std::net::Shutdown;
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+
+    while match stream.read(&mut data){
+        Ok(size) => {
+            // echo everything!
+            println!("{}", std::str::from_utf8(&data).unwrap());
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            },
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+
+    drop(listener);
+    Ok(())
 }
